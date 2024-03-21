f3e7c24967c09fc6145d12cf760c5baaa70e7ac8
Megha-Hebbal <hebbal.megha@gmail.com>
2024-03-21 21:22:50 +0530

    Launch page

---
+++ js/confetti.js
@@ -0,0 +1,12 @@
+const button = document.querySelector('#button');
+const canvas = document.querySelector('#confetti');
+
+const jsConfetti = new JSConfetti();
+
+button.addEventListener('click', () => {
+    jsConfetti.addConfetti().then(() => jsConfetti.addConfetti())
+
+    setTimeout(() => {
+      window.location.href = "http://127.0.0.1:5500/Abacus-Website/index.html"; // Replace with your target URL
+    }, 6000);
+})
